f7747551a771b87210b1dc27b45770060d3a2206
Juan Victor Oliveira <juanvictoroliveira2009@gmail.com>
2021-01-26 10:22:37 -0300

    Fixed imports

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,9 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../entities/Appointment';
+import AppError from '@shared/errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
-import AppError from '../../../shared/errors/AppError';
 
 // TODO SERVICE TEM UM ÚNICO MÉTODO
 // Não tem acesso aos dados da requisitação e resposta
