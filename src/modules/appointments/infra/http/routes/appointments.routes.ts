c2633adf495f6d9b9f28268e6dd8e89e474a8975
Juan Victor Oliveira <juanvictoroliveira2009@gmail.com>
2021-01-26 12:28:53 -0300

    Adding ddd architecture

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,27 +1,25 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 // Rota: Receber requisição, chamar outro arquivo, devolver uma resposta // Trabalha o dado
 
 const appointmentsRouter = Router();
-
+const appointmentsRepository = new AppointmentsRepository();
 /*
     É verificada a autenticação em todas as rotas
     Caso queira aplicar a verificação em rotas específicas, use ex: get('/', ensureAuthenticated, async ...)
 */
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+/* appointmentsRouter.get('/', async (request, response) => {
     const appointments = await appointmentsRepository.find();
 
     return response.json(appointments);
-});
+}); */
 
 // POST https://localhost:3333/apointments
 appointmentsRouter.post('/', async (request, response) => {
@@ -29,7 +27,9 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService(
+        appointmentsRepository,
+    );
 
     const appointment = await createAppointment.execute({
         date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+// import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 // TODO SERVICE TEM UM ÚNICO MÉTODO
 // Não tem acesso aos dados da requisitação e resposta
@@ -13,19 +13,21 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
  * Acesso ao repositório
  */
 
-interface Request {
+interface IRequest {
     provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ date, provider_id }: Request): Promise<Appointment> {
-        const appointmentsRepository = getCustomRepository(
-            AppointmentsRepository,
-        );
+    constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
+    public async execute({
+        date,
+        provider_id,
+    }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
         );
 
@@ -33,12 +35,10 @@ class CreateAppointmentService {
             throw new AppError('this appointment is already booked');
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await this.appointmentsRepository.create({
             date: appointmentDate,
             provider_id,
         });
-
-        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
