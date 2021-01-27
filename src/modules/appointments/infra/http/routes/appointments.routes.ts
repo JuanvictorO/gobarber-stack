2a54d94dceb68e20ca3a9e11ad4cf1f68894201d
Juan Victor Oliveira <juanvictoroliveira2009@gmail.com>
2021-01-27 16:58:32 -0300

    Added controllers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 // Rota: Receber requisição, chamar outro arquivo, devolver uma resposta // Trabalha o dado
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 /*
     É verificada a autenticação em todas as rotas
     Caso queira aplicar a verificação em rotas específicas, use ex: get('/', ensureAuthenticated, async ...)
@@ -21,19 +19,6 @@ appointmentsRouter.use(ensureAuthenticated);
 }); */
 
 // POST https://localhost:3333/apointments
-appointmentsRouter.post('/', async (request, response) => {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-        date: parsedDate,
-        provider_id,
-    });
-
-    return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
